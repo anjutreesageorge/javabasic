4a6e7037dc5d587554bcaa9f74d3afcae140602a
anjutreesageorge <41456272+anjutreesageorge@users.noreply.github.com>
2019-10-22 18:35:32 -0400

    first version

--- src/main/java/com/busyqa/coursework/wk1_answer/SumAndAverage.java
+++ src/main/java/com/busyqa/coursework/wk1_answer/SumAndAverage.java
@@ -1,5 +1,6 @@
 package com.busyqa.coursework.wk1_answer;
 
+
 public class SumAndAverage {
     public static void main (String[] args) {
         int sum = 0;          // Store the accumulated sum, init to 0
@@ -13,5 +14,9 @@ public class SumAndAverage {
         }
         // Compute average in double. Beware that int/int produces int.
         // Print sum and average.
+        average = (double)sum/upperbound;
+        
+        System.out.println("The sum is "+sum);
+        System.out.println("The average is "+average);
     }
 }
